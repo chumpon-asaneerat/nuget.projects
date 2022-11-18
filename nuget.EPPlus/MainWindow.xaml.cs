9c2345ef17abb1e0787ce38852c7d8240e415c48
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-19 06:44:25 +0700

    nuget.projects  #11 - Add nuget.EPPlus project.   - Add Microsoft.IO.RecyclableMemoryStream latest package   - Update App.Config

---
+++ nuget.EPPlus/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+#endregion
+
+using OfficeOpenXml;
+using System.IO;
+
+namespace nuget.EPPlus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructor
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Run();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void Run()
+        {
+            // If you are a commercial business and have
+            // purchased commercial licenses use the static property
+            // LicenseContext of the ExcelPackage class :
+            ExcelPackage.LicenseContext = LicenseContext.Commercial;
+
+            // If you use EPPlus in a noncommercial context
+            // according to the Polyform Noncommercial license:
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            /*
+            using (var package = new ExcelPackage(new FileInfo("MyWorkbook.xlsx")))
+            {
+
+            }
+            */
+        }
+
+        #endregion
+    }
+}
